f3867d556f70088bbb9872f3791ce87b0005a738
Davide-Ficocelli <davide.ficocelli.dev@gmail.com>
2025-09-03 10:09:42 +0200

    Added error handling

--- components/Header.jsx
+++ components/Header.jsx
@@ -2,6 +2,12 @@ import React from "react";
 import { Link, NavLink } from "react-router-dom";
 
 export default function Header() {
+  const activeStyles = {
+    fontWeight: "bold",
+    textDecoration: "underline",
+    color: "#161616",
+  };
+
   return (
     <header>
       <Link className="site-logo" to="/">
@@ -10,22 +16,25 @@ export default function Header() {
       <nav>
         <NavLink
           to="/host"
-          className={({ isActive }) => (isActive ? "active-link" : null)}
+          style={({ isActive }) => (isActive ? activeStyles : null)}
         >
           Host
         </NavLink>
         <NavLink
           to="/about"
-          className={({ isActive }) => (isActive ? "active-link" : null)}
+          style={({ isActive }) => (isActive ? activeStyles : null)}
         >
           About
         </NavLink>
         <NavLink
           to="/vans"
-          className={({ isActive }) => (isActive ? "active-link" : null)}
+          style={({ isActive }) => (isActive ? activeStyles : null)}
         >
           Vans
         </NavLink>
+        <Link to="login" className="login-link">
+          <img src="../assets/images/avatar-icon.png" className="login-icon" />
+        </Link>
       </nav>
     </header>
   );
